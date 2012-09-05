22bdf820ee70cef2dfadbfc65a7bb9e604f0cf35
Manuel Stadelmann <manuel@allotria.ch>
2012-09-05 19:53:28 +0200

    new version of XFLamp. Uses Serial.parseInt() and Serial.findUntil() to parse the input.

--- XFLamp/XFLamp.ino
+++ XFLamp/XFLamp.ino
@@ -1,47 +1,53 @@
 /*
   XFspöka project
-  ---------------
-  XFLamp sketch
-  
-  Reads values from serial port and lights up two RGB LEDs accordingly.
+ ---------------
+ XFLamp sketch
  
-  XFspöka by Manuel Stadelmann is licensed under a Creative Commons 
-  Attribution-ShareAlike 3.0 Unported License (CC BY-SA).
-  
+ Reads values from serial port and lights up two RGB LEDs accordingly.
+ 
+ Notes:
+ ------
+ Connect two common cathode RGB LEDs to the PWM pins as below:
+ +-----+-------+-------------+
+ | LED | color | arduino pin |
+ +-----+-------+-------------+
+ |     | red   |       3     |
+ |  1  | green |       5     |
+ |     | blue  |       6     |
+ +-----+-------+-------------+
+ |     | red   |      11     |
+ |  2  | green |      10     |
+ |     | blue  |       9     |
+ +-----+-------+-------------+  
+ 
+ Licenses:
+ ---------
+ XFspöka by Manuel Stadelmann is licensed under a Creative Commons 
+ Attribution-ShareAlike 3.0 Unported License (CC BY-SA).
+ 
  */
- 
-// The length of the serial input buffer
-#define BUFFER_LEN 6
-#define SERIAL_READ_DELAY 10
- 
+
 // PWM pins for LED 1
 static int LED1_RED = 3;
-static int LED1_BLUE = 5;
-static int LED1_GREEN = 6;
+static int LED1_GREEN = 5;
+static int LED1_BLUE = 6;
 
 // PWM pins for LED 2
-static int LED2_RED = 9;
-static int LED2_BLUE = 10;
-static int LED2_GREEN = 11;
+static int LED2_RED = 11;
+static int LED2_GREEN = 10;
+static int LED2_BLUE = 9;
 
+static int pins[] = {
+  LED1_RED, LED1_GREEN, LED1_BLUE, LED2_RED, LED2_GREEN, LED2_BLUE};
+static int pinArrayLength = 6;
 
-// read buffer and helper variables for serial communication
-int serialIn;
-int serialBuffer[ BUFFER_LEN ];
-int serialIndex = 0;
 
 // setup the output pins and the serial port
 void setup() {                
-  // initialize the LED 1 pins
-  pinMode(LED1_RED, OUTPUT);
-  pinMode(LED1_GREEN, OUTPUT);
-  pinMode(LED1_BLUE, OUTPUT);
-  
-  // initialize the LED 2 pins
-  pinMode(LED2_RED, OUTPUT);
-  pinMode(LED2_GREEN, OUTPUT);
-  pinMode(LED2_BLUE, OUTPUT);
-  
+  for (int i = 0; i < pinArrayLength; i = i+1) {
+    pinMode(pins[i], OUTPUT);
+  }
+
   // Start serial communication
   Serial.begin(9600);
   Serial.println("Hello, I'm ready");
@@ -51,45 +57,48 @@ void loop() {
 
   // see if new values are available on the serial port
   if (Serial.available() > 0) {
-     
-    // read all available bytes from the serial port 
-    // as long as they fit into the buffer
-    while(Serial.available() > 0 && serialIndex < BUFFER_LEN) {
-      // read ready byte from serial port
-      serialIn = Serial.read();
-      
-      // store to the buffer
-      serialBuffer[serialIndex++] = serialIn;
-       
-      // give the arduino some rest to be 
-      // able to receive new serial data ;-)
-      delay(SERIAL_READ_DELAY);
+    Serial.println("data read");
+    int red1 = Serial.parseInt();
+    int green1 = Serial.parseInt();
+    int blue1 = Serial.parseInt();
+    int colorsOne[] = {constrain(red1, 0, 255), constrain(green1, 0, 255), constrain(blue1, 0, 255)};
+
+    int red2 = Serial.parseInt();
+    int green2 = Serial.parseInt();
+    int blue2 = Serial.parseInt();
+    int colorsTwo[] = {constrain(red2, 0, 255), constrain(green2, 0, 255), constrain(blue2, 0, 255)};
+
+    // look for the newline. That's the end of your
+    // sentence:
+    if (Serial.findUntil("end", "\n")) {
+      Serial.println("terminator");
+      // write color values to the LEDs
+      lightUpLed(0, colorsOne);
+      lightUpLed(1, colorsTwo);
     }
-    
-    // write color values to the LEDs
-    lightUpLed1(serialBuffer);
-    lightUpLed2(serialBuffer);
-    
-    // reset the buffer index to the first byte
-    serialIndex = 0;
   }
 }
 
 /*
- * Let the LED1 shine with the color values from the first three bytes of the serialBuffer.
+ * Let the given led shine in the given colors.
+ * The first led is 0 the other 1.
  */
-void lightUpLed1(int serialBuffer[]) {
-  analogWrite(LED1_RED, serialBuffer[0]);
-  analogWrite(LED1_GREEN, serialBuffer[1]);
-  analogWrite(LED1_BLUE, serialBuffer[2]);
+void lightUpLed(int led, int colors[]) {
+  int offset = led * 3;
+  for (int i = 0; i < 3; i = i+1) {
+    analogWrite(pins[offset + i], colors[i]);
+    Serial.print("LED ");
+    Serial.println(led);
+    Serial.print("color ");
+    Serial.println(i);
+    Serial.print("value ");
+    Serial.println(colors[i]);
+    Serial.println("");
+  }
 }
 
 
-/*
- * Let the LED2 shine with the color values from the last three bytes of the serialBuffer.
- */
-void lightUpLed2(int serialBuffer[]) {
-  analogWrite(LED2_RED, serialBuffer[3]);
-  analogWrite(LED2_GREEN, serialBuffer[4]);
-  analogWrite(LED2_BLUE, serialBuffer[5]);
-}
+
+
+
+
